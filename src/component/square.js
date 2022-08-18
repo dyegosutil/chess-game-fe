77dd5c0616a3f2ada3074babc10bc35c82583fb0
beckertobias <tobiasniklasbecker@gmail.com>
2022-08-18 16:44:26 +0200

    load img correctly

--- src/component/square.js
+++ src/component/square.js
@@ -1,4 +1,4 @@
-import horseImage from "../images/horse_chess.png"
+import horseImage from "../images/5.png";
 
 export const Square = ({ color, height, width }) => {
   return (
@@ -9,9 +9,14 @@ export const Square = ({ color, height, width }) => {
         width: width,
         border: "1px solid black",
         boxSizing: "border-box",
+
+        // to align the figures in the center of the square
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-        <img src="../images/horse_chess.png" alt={"shit happened"}></img>
+      <img src={horseImage} alt={"shit happened"} width={width * 0.8}></img>
     </div>
   );
 };
